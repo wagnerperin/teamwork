8436b15311d0a55a93eb651932d231f4924ea93d
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2022-04-29 18:23:26 -0300

    Modificando a pagina de login

--- views/login.php
+++ views/login.php
@@ -8,30 +8,37 @@
  <div class="container-fluid">
   <div class="container">
    <div class="row">
-   <div class="col-lg-3"></div> 
-    <div class="col-lg-6">
-      <h1 class="mt-5 text-center"> Login </h1>
+    <div class="col-lg-12">
+     <h3 class="pt-3"><b class="text-secondary"> Login </b></h3>
+     <hr>
 
-      <form action="back-end/controllers/createUser.php" method="POST" class="row g-2" enctype="multipart/form-data">
-
-       <div class="col-md-12 input-group-sm font-size-register">
-        <label for="email" class="form-label">E-mail</label>
-        <input type="email" class="form-control form-control-md text-dark" id="email" name="email" required>
-       </div><br>
-
-       <div class="col-md-12 mt-3 input-group-sm font-size-register">
-        <label for="password" class="form-label">Senha</label>
-        <input type="password" class="form-control form-control-md text-dark password" id="password" name="password" required>
+      <form class="g-3 needs-validation pt-3" novalidate>
+       <div class="col-md-6 pt-3">
+        <label for="" class="form-label"> E-mail </label>
+        <input type="email" class="form-control" id="" placeholder="Seu email"  required>
+        <div class="valid-feedback"> Tudo ok </div>
        </div>
 
-       <div class="col-12 input-group-sm mt-4 d-flex justify-content-center">
-        <input type="submit" class="btn btn-primary btn-block p-5 pt-2 pb-2 mb-3 font-size-register" value="LOGIN" style="border-radius: 10px;">
+       <div class="col-md-6 pt-3">
+        <label for="" class="form-label"> Senha </label>
+        <input type="password" class="form-control" id="" required>
+        <div class="invalid-feedback"> Por favor corrija sua senha. </div>
        </div>
 
-     </form> 
+       <div class="col-12 pt-3">
+        <div class="form-check">
+         <input class="form-check-input" type="checkbox" value="" id="" required>
+         <label class="form-check-label" for=""> Lembre-me </label>
+         <div class="invalid-feedback"> Confira seus dados. </div>
+        </div>
+       </div>
+
+       <div class="col-12 pt-3">
+        <button class="btn btn-primary" type="submit"> Logar </button>
+       </div>
+      </form>
      
     </div>
-    <div class="col-lg-3"></div> 
    </div>   
   </div>   
  </div>
