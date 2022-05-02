162d66a9296b7e94ed3ee69ba08c7607427d02b1
MillenySoares <moreiramilleny8@gmail.com>
2022-05-02 15:20:45 -0300

    pagina UserDAO

---
+++ models/DAO/UserDAO.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("../config/Banco.php");
+
+    class UserDAO {
+        private static $instance;
+
+        public static function getInstance() {
+            if(self::$instance === null) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+
+        public function save(User $user) {
+            $stmt = Banco::getInstance()->prepare("INSERT INTO users(name, email, cpf, password, userTyper,	createdAt) VALUES (:name, :email, :cpf, :password, :userTyper, :createdAt)");
+            $stmt->bindParam("name", $user->name);
+            $stmt->bindParam("email", $user->email);
+            $stmt->bindParam("cpf", $user->cpf);
+            $stmt->bindParam("password", $user->password);
+            $stmt->bindParam("userTyper", $user->userTyper);
+            $stmt->bindParam("createdAt", $user->createdAt);
+
+            $stmt->execute();
+        }
+        
+    }
+
+?>
