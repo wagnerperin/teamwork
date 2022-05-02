0dcf91f61d5bbab169112e0fa8200af8ced78dad
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2022-05-02 15:20:54 -0300

    Trabalhando no Controllers

---
+++ controllers/cadastro.php
@@ -0,0 +1,12 @@
+<?php
+    
+    require_once("models/User.php");
+    require_once("DAO/UserDAO.php");
+    
+    UserDAO::getInstance()->save(new User($_POST['name'], $_POST['email'], $_POST['cpf'], $_POST['password']));
+
+    session_start();
+    $_SESSION['new_user'] = $newUser->name;
+    header("Location: index.php");
+
+?>
--- views/cadastro.php
+++ views/cadastro.php
@@ -12,17 +12,17 @@ include "../inc/menu.php";
       </div>
 
       <div>
-        <form class="g-3 needs-validation pt-3" novalidate>
+        <form class="g-3 needs-validation pt-3" novalidate method="POST" action="../controllers/cadastro.php">
           <div class="col-md-4">
             <label for="validationCustom01" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="validationCustom01" placeholder="Seu Nome" required>
+            <input name="name" type="text" class="form-control" id="validationCustom01" placeholder="Seu Nome" required>
             <div class="valid-feedback">
               Tudo ok
             </div>
           </div>
           <div class="col-md-4 pt-3">
             <label for="validationCustom02" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="validationCustom02" placeholder="Seu email"  required>
+            <input name="email" type="email" class="form-control" id="validationCustom02" placeholder="Seu email"  required>
             <div class="valid-feedback">
               Tudo ok
             </div>
@@ -30,7 +30,7 @@ include "../inc/menu.php";
           <div class="col-md-4 pt-3">
             <label for="validationCustomUsername" class="form-label">CPF</label>
             <div class="input-group has-validation">
-              <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+              <input name="cpf" type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
               <div class="invalid-feedback">
                 Por favor coloque seu CPF.
               </div>
@@ -38,7 +38,7 @@ include "../inc/menu.php";
           </div>
           <div class="col-md-6 pt-3">
             <label for="validationCustom03" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="validationCustom03" required>
+            <input name="password" type="password" class="form-control" id="validationCustom03" required>
             <div class="invalid-feedback">
               Por favor corrija sua senha.
             </div>
