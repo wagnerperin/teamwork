c91cd7467bdbc0a359641d98d920e2320d731581
MillenySoares <moreiramilleny8@gmail.com>
2022-05-02 14:21:48 -0300

    pagina inicial

--- index.php
+++ index.php
@@ -4,42 +4,218 @@
 ?>
 <div class="container mt-5">
   <div class="row">
-    <div class="col-sm-4">
-      <h2>About Me</h2>
-      <h5>Photo of me:</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <h3 class="mt-4">Some Links</h3>
-      <p>Lorem ipsum dolor sit ame.</p>
-      <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <hr class="d-sm-none">
+    <div class="col-3">
+      <p>
+        <a style="text-decoration: none;" class="text-secondary" data-bs-toggle="collapse" href="#collapseExample0" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <h4>Desenvolvimento</h4>
+        </a>
+      </p>
+      <div class="collapse" id="collapseExample0">
+        <div class="card card-body">
+          <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample1">
+              Linguagem de programação
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample1">
+            <div class="card card-body">
+              <a class="link-dark" href="#">Python</a> 
+              <a class="link-dark" href="#">Java </a>
+              <a class="link-dark" href="#">C#</a>
+              <a class="link-dark" href="#">React</a>
+              <a class="link-dark" href="#">C++</a>
+              <a class="link-dark" href="#">JavaScript</a>
+              <a class="link-dark" href="#">Go (linguagem de programação)</a>
+              <a class="link-dark" href="#">C (linguagem de programação)</a>
+            </div>
+          </div>
+          <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Desenvolvimento de games
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample2">
+            <div class="card card-body">
+              <a class="link-dark" href="#">Unity</a> 
+              <a class="link-dark" href="#">Unreal Engine</a>
+              <a class="link-dark" href="#">Fundamentos do Desenvolvimento de games</a>
+              <a class="link-dark" href="#">C#</a>
+              <a class="link-dark" href="#">Desenvolvimento de games 3D</a>
+              <a class="link-dark" href="#">C++</a>
+              <a class="link-dark" href="#">Desenvolvimento de games 2D</a>
+              <a class="link-dark" href="#">Blueprints da Unreal Engine</a>
+              <a class="link-dark" href="#">Realidade Virtual</a>
+            </div>
+          </div>
+          <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Teste de software
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample3">
+            <div class="card card-body">
+              <a class="link-dark" href="#">Selenium WebDriver</a> 
+              <a class="link-dark" href="#">Testes de automação</a>
+              <a class="link-dark" href="#">Java</a>
+              <a class="link-dark" href="#">Selenium Testing Framework</a>
+              <a class="link-dark" href="#">Teste de API</a>
+              <a class="link-dark" href="#">Postman</a>
+              <a class="link-dark" href="#">Avaliação de qualidade</a>
+              <a class="link-dark" href="#">Cypress.io</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>
+        <a style="text-decoration: none;" class="text-secondary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <h4 class="mt-3">Design‌</h4>
+        </a>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+            <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Web design
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample4">
+            <div class="card card-body">
+              <a class="link-dark" href="#">WordPress</a> 
+              <a class="link-dark" href="#">Design de aplicativos móveis</a>
+              <a class="link-dark" href="#">CSS</a>
+              <a class="link-dark" href="#">HTML</a>
+              <a class="link-dark" href="#">Interface de usuário</a>
+              <a class="link-dark" href="#">Photoshop</a>
+              <a class="link-dark" href="#">HTML5</a>
+              <a class="link-dark" href="#">Adobe XD</a>
+            </div>
+          </div>
+          <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample5" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Design interior
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample5">
+            <div class="card card-body">
+              <a class="link-dark" href="#">SketchUP</a> 
+              <a class="link-dark" href="#">Teoria das cores</a>
+              <a class="link-dark" href="#">Design de iluminação</a>
+              <a class="link-dark" href="#">AVAC (aquecimento, ventilação e ar condicionado)</a>
+              <a class="link-dark" href="#">Engenharia Elétrica</a>
+              <a class="link-dark" href="#">Feng Shui</a>
+              <a class="link-dark" href="#">Preparação de casa</a>
+              <a class="link-dark" href="#">Eletricidade</a>
+            </div>
+          </div>
+          <p>
+            <a class="text-secondary " data-bs-toggle="collapse" href="#collapseExample6" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Design de games
+            </a>
+          </p>
+          <div class="collapse" id="collapseExample6">
+            <div class="card card-body">
+              <a class="link-dark" href="#">Unreal Engine</a> 
+              <a class="link-dark" href="#">Pixel Art</a>
+              <a class="link-dark" href="#">Unity</a>
+              <a class="link-dark" href="#">VFX Visual Elfects</a>
+              <a class="link-dark" href="#">Blender</a>
+              <a class="link-dark" href="#">Pintura digital</a>
+              <a class="link-dark" href="#">Fundamentos do Desenvolvimentode games</a>
+              <a class="link-dark" href="#">Level design</a>
+              <a class="link-dark" href="#">Spring Framework</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2020</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <div class="col-9">
+      <div class="row">
+        <div class="col-3">
+          <div class="card" style="width: 15rem;height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/4152006_54df_5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">Python Django 2021 - Complete Course</b><br>
+              <span style="font-size:13px" class="text-black-50">Victor Almeida</span><br>
+              <b>R$ 99,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card" style="width: 15rem;height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/720750_6cba_8.jpg" alt="...">
+            <div class="card-body">
+              <b style="" class="card-text">Curso Completo JAVA para Iniciantes</b><br>
+              <span style="font-size:13px" class="text-black-50">Silvana Aguiar</span><br>
+              <b>R$ 179,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card" style="width: 15rem; height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/1048978_1887_3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">Linguagem de programação C# - Básico</b><br>
+              <span style="font-size:13px" class="text-black-50">Júlio Santana</span><br>
+              <b class="">R$ 79,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" style="width: 15rem; height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/2255806_2642.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">React, Redux e integração de APIs</b><br>
+              <span style="font-size:13px" class="text-black-50">Natan Oliveira</span><br>
+              <b>R$ 79,90</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-3">
+          <div class="card" style="width: 15rem;height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/619698_0efe.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">Learn Advanced C++ Programming</b><br>
+              <span style="font-size:13px" class="text-black-50">Juliana Alves</span><br>
+              <b>R$ 89,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card" style="width: 15rem;height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/999200_ae6d_4.jpg" alt="...">
+            <div class="card-body">
+              <b style="" class="card-text">Introduction to TypeScript Development</b><br>
+              <span style="font-size:13px" class="text-black-50">Guilherme Santos</span><br>
+              <b>R$ 99,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card" style="width: 15rem; height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/1309202_1231_4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">Concurrency in Go (Golang)</b><br>
+              <span style="font-size:13px" class="text-black-50">Roque Soares</span><br>
+              <b class="">R$ 179,90</b>
+            </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" style="width: 15rem; height: 264px;">
+            <img src="https://img-b.udemycdn.com/course/240x135/849236_792f_2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <b class="card-text">Aprenda e Domine a Linguagem C</b><br>
+              <span style="font-size:13px" class="text-black-50">Wanderson Oliveira</span><br>
+              <b>R$ 79,90</b>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <h2 class="mt-5">TITLE HEADING</h2>
-      <h5>Title description, Sep 2, 2020</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
     </div>
+  </div>
   </div>
 </div>
 
