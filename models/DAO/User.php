1fd3928dad431cf6f3056b9f47504f6b45e8a424
Guilherme-Trindade <guilhermetrindade34@gmail.com>
2022-05-02 15:50:28 -0300

    Adicionado criptografia concertando novamente

--- models/DAO/User.php
+++ models/DAO/User.php
@@ -4,17 +4,17 @@ class User {
     public string $email;
     public string $cpf;
     public string $password;
-    public string $userType;
-    public string $createdAt;
+    public int $userType;
+    public DateTime $createdAt;
 
 
-    public function __construct(string $name, string $email, string $cpf, string $password, string $userType, string $createdAt){
+    public function __construct(string $name, string $email, string $cpf, string $password, int $userType){
         $this->name = $name;
         $this->email = $email;
         $this->cpf = $cpf;
         $this->password = password_hash($password, PASSWORD_BCRYPT);
         $this->userType = $userType;
-        $this->createdAt = date("Y-m-d");
+        $this->createdAt = new DateTime ("Y-m-d");
     }
     
         
